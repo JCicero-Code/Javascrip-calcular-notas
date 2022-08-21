450e88a56e50b24bd033b76c3e511d05c31a321c
JCicero-Code <109885635+JCicero-Code@users.noreply.github.com>
2022-08-21 15:06:53 -0300

    Arrow function
    Atividade sobre algumas formas de declarar functions como as arrow function

---
+++ exerArrowFunction.js
@@ -0,0 +1,34 @@
+﻿console.log("|".repeat(40))
+
+console.log("-----Function normal-----")
+
+function soma(){
+    
+    return 2 + 2;
+    
+}
+
+console.log(soma());
+
+
+console.log("-----Com parâmetros--------")
+
+function somaDois(num1,num2){
+    
+    resposta = num1 + num2
+    return resposta
+ 
+}
+
+console.log(somaDois(8,8))
+
+
+console.log("-----Arrow function--------")
+
+let somaTres = () => 5 + 12
+
+console.log(somaTres())
+
+
+console.log("|".repeat(40))
+
